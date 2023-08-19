a2e4784331c7705a036063ca6205a86b48f05779
mindfulgaze <gaimzzpol@gmail.com>
2023-08-19 22:29:27 +0200

    add custom dataset for guanaco

--- db_test.py
+++ db_test.py
@@ -1,48 +1,19 @@
-from dotenv import load_dotenv
-import os
 import json
-import psycopg2
 
-# Load environment variables from .env file
-load_dotenv()
+with open('ShareGPT_V3_unfiltered_cleaned_split.json') as f:
+        dataset = json.load(f)
+    # Filter out the conversations with less than 2 turns.
+dataset = [
+    data for data in dataset
+    if len(data["conversations"]) >= 2
+]
+# Only keep the first two turns of each conversation.
+dataset = [
+    (data["conversations"][0]["value"], data["conversations"][1]["value"])
+    for data in dataset
+]
 
-# Replace [YOUR-PASSWORD] with your actual password
-db_config = {
-    "user": "postgres",
-    "password": os.getenv('db-pwd'),
-    "host": os.getenv('db-host'),
-    "port": os.getenv('db-port'),
-    "database": "postgres",
-}
-
-# Connect to the database
-connection = psycopg2.connect(**db_config)
-cursor = connection.cursor()
-
-# Define the table name and column name
-table_name = "test"
-column_name1 = "text"
-column_name2 = "type"
-
-# Insert data into the database
-def insert_into_db(action, type):
-    try:
-        query = f"INSERT INTO {table_name} ({column_name1}, {column_name2}) VALUES (%s, %s)"
-        cursor.execute(query, (action, type))
-        connection.commit()
-    except Exception as e:
-        print("Error inserting data into the database:", e)
-        connection.rollback()
-
-# Example usage
-
-prompt_answer_log = {
-    "prompt": "This is the prompt",
-    "answer": "OUTPUT"
-}
-
-insert_into_db(json.dumps(prompt_answer_log), "prompt_answer")
-
-# Close the database connection
-cursor.close()
-connection.close()
+# Tokenize the prompts and completions.
+prompts = [prompt for prompt, _ in dataset]
+completions = [completion for _, completion in dataset]
+hola = 1 + 1
